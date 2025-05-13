b370a452219a1599da9651a517983d9410fca534
vasujain275 <vasujain275@gmail.com>
2025-05-13 23:20:47 +0700

    Update

--- install.sh
+++ install.sh
@@ -2,10 +2,10 @@
 
 # Define variables for directories and files
 USER_HOME=$(eval echo ~$SUDO_USER)
-RUDRA_DIR="$USER_HOME/rudra"
+RUDRA_DIR="$USER_HOME/nixconfig"
 
 # Run nixos-generate-config command
-sudo nixos-generate-config --show-hardware-config > "$RUDRA_DIR/hosts/default/hardware-configuration.nix" || { echo "Failed to generate hardware configuration"; exit 1; }
+sudo nixos-generate-config --show-hardware-config > "$RUDRA_DIR/hosts/william/hardware-configuration.nix" || { echo "Failed to generate hardware configuration"; exit 1; }
 
 # Navigate to home directory
 cd "$USER_HOME" || { echo "Failed to cd to home directory"; exit 1; }
@@ -14,6 +14,6 @@ cd "$USER_HOME" || { echo "Failed to cd to home directory"; exit 1; }
 cd "$RUDRA_DIR" || { echo "Failed to cd to $RUDRA_DIR"; exit 1; }
 
 # Rebuild NixOS configuration
-sudo nixos-rebuild switch --flake .#default || { echo "Failed to rebuild NixOS configuration"; exit 1; }
+sudo nixos-rebuild switch --flake .#william || { echo "Failed to rebuild NixOS configuration"; exit 1; }
 
 echo "Script completed successfully."
